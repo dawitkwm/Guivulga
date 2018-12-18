b9db9e339fb0c6a3bddfe36a3a41cef37346718c
Namchin Dechingavaa <namchin@Namchins-MacBook-Pro.local>
2018-12-17 18:17:27 -0600

    removed rank from user/populate.sql

--- src/main/java/com/codebus/domain/User.java
+++ src/main/java/com/codebus/domain/User.java
@@ -53,8 +53,8 @@ public class User implements Serializable {
 	@Column(name = "email", nullable = false)
 	private String email;
 
-	@Column(name = "rank", nullable = false)
-	private Integer rating = 0;
+//	@Column(name = "[rank]", nullable = false)
+//	private Integer rating = 0;
 
 	@Column(name = "is_admin", nullable = false)
 	private boolean admin = false;
@@ -114,13 +114,13 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
-	public Integer getRating() {
-		return rating;
-	}
-
-	public void setRating(Integer rating) {
-		this.rating = rating;
-	}
+//	public Integer getRating() {
+//		return rating;
+//	}
+//
+//	public void setRating(Integer rating) {
+//		this.rating = rating;
+//	}
 
 	public boolean isAdmin() {
 		return admin;
